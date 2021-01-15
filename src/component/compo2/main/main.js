3c7b1abcbc5045009bcf2b3641bc3715b780ca39
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2021-01-15 02:43:31 -0600

    about

--- src/component/compo2/main/main.js
+++ src/component/compo2/main/main.js
@@ -5,16 +5,17 @@ function Main() {
   return (
     <section>
     <div  className='container-box-2'>
-          <img style={{width:'250px',textAlign:'center'}}  alt ='' src={work}/>
-          <hr className='line'/>
+          <img   alt ='' src={work}/>
           <p className='parraph'><i class="fab fa-html5">
           </i>HTML/CSS<i className=" fab fa-css3-alt">
           </i>|JavaScript<i class="fab fa-node-js">
           </i>|React-Redux<i class="fab fa-react"></i>|
           </p>
+          <hr className='line'/>
           <h3>you don't climb the mountain you never got to </h3>
+          <button>Contact</button>
           <div className='socials-liks'>
-           <a className ='link' href="http://google.com" 
+           <a className ='link' href="https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/" 
                rel='noopener noreferrer' target='_black'><i 
                className=" fab fa-linkedin"></i></a>
            <a className ='link' href="https://github.com/barbaroreyes" 
@@ -24,6 +25,8 @@ function Main() {
               rel='noopener noreferrer' target='_black'>
              <i className="  fab fa-facebook-square"></i></a>
           </div>
+          
+          <hr className='line'/>
           
       </div>
    </section>
