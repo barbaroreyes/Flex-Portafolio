90297d07edbdb2f0c79b621ede16558d3e3a2462
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2021-01-15 01:44:51 -0600

    fixing 2nd component

--- src/component/compo2/footer/footer.js
+++ src/component/compo2/footer/footer.js
@@ -5,9 +5,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className='container-box-2'>
-    <h3>box seven</h3>
-     
-      
+    <h3>box seven</h3>  
     </div>
   )
 }
