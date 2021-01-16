d835f2c69a2c1467d8d8ad8bc8772578634b22ef
barbarof <barbarofreyes@gmail.com>
2021-01-16 17:03:44 -0600

    working on contact-page

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -3,15 +3,34 @@ import './contact.css';
   export default class Contact extends Component {
         render() {
             return (
-         <div className='contact-body'>
-         <div><p>With a huge desire to bring smart ideas to the Digital World</p></div>    
-    
-       
-<div className='contact-list'>
-   
-</div>
-                 
-            </div>
+                  <form className='contact'>
+                  <label >name</label>
+                  <input/>
+                  <label>Email</label>
+                  <input/>
+                       <div className='p-change'>
+                       <p className='p-change'><i class="fas fa-envelope-square">
+                       <a href='barbarotrabajo@gmail.com'>
+                       <p><a className='nav' 
+                       href="mailto:barbarotrabajo@gmail.com?subject= Hey Barbaro, I'm interested in hearing from you! "
+                       target='_black'>Email</a>
+                       </p></a></i></p></div>
+
+                       <div className='p-change'>
+                       <p className='p-change'>
+                       <i class="fab fa-github"><p>
+                       <a className='nav' href="https://github.com/barbaroreyes "
+                       target='_black'>Github</a>
+                       </p></i></p></div>
+
+                       <div className='p-change'>
+                       <p className='p-change'>
+                       <i class=" link fab fa-linkedin"><p>
+                       <a className='nav' href='https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/'
+                       target='_black'>linkedin</a>
+                       </p></i></p></div>
+            </form>
+                    
             )
         }
     }
