c4b04d8d814004a87c1c5d1e0af28fb419bd3079
barbarof <barbarofreyes@gmail.com>
2021-01-16 22:50:11 -0600

    update

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -3,22 +3,21 @@ import './contact.css';
   export default class Contact extends Component {
         render() {
             return (
-                  <form className='contact'>
-                  
+                  <form type='submit'>
                   <div className='contact'>
                   <div className ='contact-1'>
                   <label>Name:</label>
-                  <input type='text' name='name'/>
+                  <input type='text' name='name' required/>
                   <label>Email:</label>
-                  <input type='text'/>
+                  <input type='text'  required/>
                   <label>Message:</label>
-                  <textarea></textarea>
+                  <textarea rows="6" cols="22" required></textarea>
                   <button className='nav' type="submit">Submit</button>
                   </div>
                   </div>
                
                        <div className='p-change'>
-                       <p className='p-change'><i class="fas fa-envelope-square">
+                       <p className='p-change one'><i class="fas fa-envelope-square">
                        <a href='barbarotrabajo@gmail.com'>
                        <p><button><a className='nav' 
                        href="mailto:barbarotrabajo@gmail.com?subject= Hey Barbaro, I'm interested in hearing from you! "
@@ -26,13 +25,13 @@ import './contact.css';
                        </p></a></i></p></div>
 
                        <div className='p-change'>
-                       <p className='p-change'>
+                       <p className='p-change one'>
                        <i class="fab fa-github"><p>
                        <button><a className='nav' href="https://github.com/barbaroreyes " target='_black'>Github</a></button>
                        </p></i></p></div>
 
-                       <div className='p-change'>
-                       <p className='p-change'>
+                       <div className='p-change  '>
+                       <p className='p-change one '>
                        <i class=" link fab fa-linkedin"><p>
                        <button><a className='nav' href='https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/'
                        target='_black'>linkedin</a></button>
