cd62f9aa4b4edec2366e45e76a68b103346b1e44
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-16 21:56:09 -0600

    update

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function App() {
   return (
     <div className="container">
       <Switch>
+      <Header/>
         <Route exact path ='/'>
-        <Header/>
+       
         <Main/>
         </Route>
          <Route exact path ='/about'>
